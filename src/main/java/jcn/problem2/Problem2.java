7052f88f605bf187105628adb6df640717b7640a
jason.norman <jason.norman@quikq.com>
2019-06-14 12:23:12 -0500

    Adding Problem 2.

--- src/main/java/jcn/problem2/Problem2.java
+++ src/main/java/jcn/problem2/Problem2.java
@@ -4,12 +4,9 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Problem2 {
-
-    private Predicate<String> hasLengthOfFive = s -> s.length() == 5;
 
     public List<String> generateWordListFromFile() {
 
